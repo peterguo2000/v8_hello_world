88d3512296aea0fc6306155ef4582b7c755b87cf
peterguo2000 <peter.k.guo@gmail.com>
2019-07-26 12:51:24 +1000

    Add files via upload

---
+++ build/shell/buildit.sh
@@ -0,0 +1,1 @@
+cd /root/shared/v8_app/app1;g++ -I. -Iinclude src/main.cpp -o out/obj/hello -lv8_monolith -Llib -pthread -std=c++0x
---
+++ out/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.dir/root/shared/v8_app/app1/src/main.cpp.o"
+  "obj/hello.pdb"
+  "obj/hello"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
